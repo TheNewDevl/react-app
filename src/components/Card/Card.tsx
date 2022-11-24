75152bb1d14555394712ea1ea926e04769db6804
carlos <carl.devfs@gmail.com>
2022-11-24 23:06:08 +0100

    fix: Navigation to lodging page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,7 +13,7 @@ const Card = ({
   title = "title",
   cover = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
 }: CardProps) => {
-  const href = `/logement/${id}`;
+  const href = `/lodging/${id}`;
 
   return (
     <article className={s.card}>
