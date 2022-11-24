4cfbb632a748c2b291461d9377f873e5f7802b62
carlos <carl.devfs@gmail.com>
2022-11-24 23:04:14 +0100

    feat: Complete Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,37 @@
+import { useEffect } from "react";
+import Banner from "../../components/Banner/Banner";
+import homeBanner from "../../assets/home-banner.png";
+import { useFetch } from "../../hooks/useFetch";
+import s from "./Home.module.scss";
+import Card from "../../components/Card/Card";
+import { ApartmentType } from "../../utils/types";
+import MainComponent from "../../layout/MainComponent";
+
 type HomeProps = {};
 
 const Home = ({}: HomeProps) => {
+  const URL = "/logements.json";
+  const { data, isLoading, error } = useFetch(URL, undefined);
+
+  useEffect(() => {
+    document.title = "Accueil";
+  }, []);
+
+  const text: JSX.Element = (
+    <h1>
+      Chez vous, <br /> partout et ailleurs
+    </h1>
+  );
+
   return (
-    <div className="Home">
-      <h1>Home</h1>
-    </div>
+    <MainComponent>
+      <Banner imgSrc={homeBanner} text={text} />
+      <section className={s.cards__container}>
+        {isLoading && <p>Chargement...</p>}
+        {error && <p>{error}</p>}
+        {data && data.map((ap: ApartmentType) => <Card key={ap.id} title={ap.title} cover={ap.cover} id={ap.id} />)}
+      </section>
+    </MainComponent>
   );
 };
 
