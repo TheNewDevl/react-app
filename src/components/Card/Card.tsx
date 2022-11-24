245e2de9cd33baf7b20d5138bfbaff80e79b2318
carlos <carl.devfs@gmail.com>
2022-11-24 22:57:10 +0100

    add: Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,31 @@
+import s from "./Card.module.scss";
+import { ApartmentType } from "../../utils/types";
+import { Link } from "react-router-dom";
+
+type CardProps = {
+  title: ApartmentType["title"];
+  cover: ApartmentType["cover"];
+  id: string;
+};
+
+const Card = ({
+  id,
+  title = "title",
+  cover = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+}: CardProps) => {
+  const href = `/logement/${id}`;
+
+  return (
+    <article className={s.card}>
+      <img src={cover} alt={`${title} illustration`} />
+      <Link to={href}>
+        <h2 className={s.card__title}>{title}</h2>
+        <span aria-hidden="true" className={s.link__mask}></span>
+      </Link>
+    </article>
+  );
+};
+
+export default Card;
+
+/** Created by carlos on 24/11/2022 */
