ec490ea1b8395837959d7563c7e6bf3c1e7b22d6
carlos <carl.devfs@gmail.com>
2022-11-25 15:34:29 +0100

    add: Complete about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,43 @@
-type AboutProps = {};
+import Banner from "../../components/Banner/Banner";
+import aboutBanner from "../../assets/about-banner.png";
+import { useEffect } from "react";
+import s from "./About.module.scss";
+import MainComponent from "../../layout/MainComponent";
+import Accordion from "../../components/Accordion/Accordion";
 
-const About = ({}: AboutProps) => {
+const AccordionList = [
+  {
+    title: "Fiabilité",
+    text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+  },
+  {
+    title: "Respect",
+    text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  },
+  {
+    title: "Service",
+    text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+  },
+  {
+    title: "Sécurité",
+    text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  },
+];
+
+const About = () => {
+  useEffect(() => {
+    document.title = "A Propos";
+  }, []);
+
   return (
-    <div className="About">
-      <h1>about</h1>
-    </div>
+    <MainComponent>
+      <Banner isFixedHeight imgSrc={aboutBanner} />
+      <div className={s.accordion__container}>
+        {AccordionList.map((item, i) => (
+          <Accordion opened={i % 2 !== 0} key={i} content={item} />
+        ))}
+      </div>
+    </MainComponent>
   );
 };
 
