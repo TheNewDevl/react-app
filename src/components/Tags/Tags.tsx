e34e97a65d331de03f9aae6b0918c56868dbed59
carlos <carl.devfs@gmail.com>
2022-11-25 23:49:51 +0100

    add: create Tags component

---
+++ src/components/Tags/Tags.tsx
@@ -0,0 +1,18 @@
+import s from "./Tags.module.scss";
+
+type TagsProps = { tagsList: string[] };
+
+const Tags = ({ tagsList }: TagsProps) => {
+  const Tag = ({ text }: { text: string }) => <span className={s.tag}>{text}</span>;
+
+  return (
+    <div className={s.tags__container}>
+      {tagsList.map((tag, index) => (
+        <Tag text={tag} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Tags;
+/** Created by carlos on 25/11/2022 */
