4bc7de8b7675c9d66c740270e5c5c37bcc65267f
carlos <carl.devfs@gmail.com>
2022-11-26 15:30:56 +0100

    refactor: improve param name

--- src/utils/cssClassesFns.ts
+++ src/utils/cssClassesFns.ts
@@ -4,8 +4,8 @@ export const handleArrowClassName = (s: StyleModule, open: boolean): string => {
   return open ? `${s.accordion__header_control} ${s.rotate}` : `${s.accordion__header_control}`;
 };
 
-export const handleContentClassName = (s: StyleModule, open: boolean, isLarge?: boolean): string => {
-  return isLarge ? `${s.accordion__content_text} ${s.accordion__content_text_small}` : `${s.accordion__content_text}`;
+export const handleContentClassName = (s: StyleModule, open: boolean, isSmall?: boolean): string => {
+  return isSmall ? `${s.accordion__content_text} ${s.accordion__content_text_small}` : `${s.accordion__content_text}`;
 };
 
 export const handleBannerClass = (highest: boolean | undefined, s: StyleModule): string => {
